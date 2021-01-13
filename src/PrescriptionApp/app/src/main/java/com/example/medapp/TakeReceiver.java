4537a0b7ec37b13038edd7cc30b9b480ee139f38
AllyInnes <allyinnes@hotmail.com>
2021-01-13 15:13:23 +0000

    can now take medication directly from notif. via action btn

--- src/PrescriptionApp/app/src/main/java/com/example/medapp/TakeReceiver.java
+++ src/PrescriptionApp/app/src/main/java/com/example/medapp/TakeReceiver.java
@@ -10,16 +10,13 @@ public class TakeReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
-        Toast.makeText(context, "test", Toast.LENGTH_SHORT).show();
-        /*
         DatabaseHelper databaseHelper = new DatabaseHelper(context);
         int doseID = intent.getIntExtra("doseID", 0);
         int medID = intent.getIntExtra("medID", 0);
         DoseModel doseModel = databaseHelper.selectDoseFromID(doseID);
         MedicationModel medModel = databaseHelper.selectMedicationFromID(medID);
-
-        //databaseHelper.takeMedication(doseModel, medModel);
-        */
+        databaseHelper.takeMedication(doseModel, medModel);
+        String msg = "You have taken " + doseModel.getAmount() + " of " + medModel.getName();
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
     }
 }
