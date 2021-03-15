cc400f2a627c5498904a354b84fbb85fe04de36f
AllyInnes <allyinnes@hotmail.com>
2021-03-15 19:09:10 +0000

    fix bug

--- src/PrescriptionApp/app/src/main/java/com/example/medapp/GoogleCalendarHelper.java
+++ src/PrescriptionApp/app/src/main/java/com/example/medapp/GoogleCalendarHelper.java
@@ -432,9 +432,8 @@ public class GoogleCalendarHelper {
         // Set a refill reminder event to remind users to refill their medication before it becomes empty
         if (medModel.getDaysUntilEmpty() >= refillReminderDays) {
             c.add(Calendar.DATE, -refillReminderDays);
-            String msg = String.format(medModel.getName() + " will run out of supply in %d days. Please order a new prescription.", refillReminderDays);
             Event refillEvent = new Event()
-                    .setDescription(getRefillMsg())
+                    .setDescription(getRefillMsg(medModel))
                     .setSummary("MedApp: " + medModel.getName() + " Refill Reminder");
             setTime(c, refillEvent);
             if (contact != null) {
